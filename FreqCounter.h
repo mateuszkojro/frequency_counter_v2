d45adba78d3c05d17ea88a99b7bbcadfa42c51c9
Mateusz Kojro <mateuszkojro@outlook.com>
2021-02-06 00:50:29 +0100

    Adding some comments

--- FreqCounter.h
+++ FreqCounter.h
@@ -1,78 +1,83 @@
-//
-// Created by mateusz on 2/2/2021.
-//
-//
-
 #ifndef FREQUENCY_COUNTER_V2_FREQCOUNTER_H
 #define FREQUENCY_COUNTER_V2_FREQCOUNTER_H
 
 #include <unordered_map>
-#include <string>
 
-typedef std::string Csv;
+#include "Csv.h"
+
+/**
+        @author: Mateusz Kojro
+        @details: Klasa freq counter - liczenie czestotliwosci wystepowania obiektow
+        @date: 27.01.2021
+        @changes:
+                - 29.01: usuwam nie potrzebne elementy z poprzedniej
+                - 31.01: dodaje dodatkowe komentarze
+                - 3.02: fixing small mistakes
+*/
 
 // Dzieki utworzeniu klasy bazowej
 // moge tworzyc w bardziej elegancki sposob
 // specjalizacje klasy FreqCounter
 template<class Type>
-class BaseFreqCounter {
+class FreqCounter {
 public:
     using CountedValues = std::unordered_map<Type, unsigned>;
 
 public:
-    BaseFreqCounter() = default;
-
-    ~BaseFreqCounter() = default;
-
-    BaseFreqCounter &operator=(const BaseFreqCounter &) = default;
-
-    BaseFreqCounter(const BaseFreqCounter &) = default;
+    FreqCounter() = default;
+    ~FreqCounter() = default;
+    FreqCounter &operator=(const FreqCounter &) = default;
+    FreqCounter(const FreqCounter &) = default;
 
 public:
     void add_data(Type);
 
     Csv export_to_csv(std::wstring (*transformer)(Type) = std::to_wstring);
 
-    // policzone wartosci zwracamy jako mape
-    // gdzie klucz to element a wartosc to liczba powtorzen
-    CountedValues get_count();
+    /**
+     * @brief calculates the number of occureneces of given class element
+     * @return std::unordered_map<Type T, unsigned num> where T is an element
+     * and the num is number of occurences of that elelment
+     */
+    const std::unordered_map<Type, unsigned int> &get_count();
 
 protected:
     CountedValues data_;
 };
 
 template<class Type>
-std::unordered_map<Type, unsigned> BaseFreqCounter<Type>::get_count() {
+const std::unordered_map<Type, unsigned int> &
+FreqCounter<Type>::get_count() {
     return data_;
 }
 
 template<class Type>
-void BaseFreqCounter<Type>::add_data(Type new_record) {
+void FreqCounter<Type>::add_data(Type new_record) {
     // std::unorderd map gwarantuje ze jezeli typ wartosci
     // jest int to bedzie on mial wartosc 0
     ++data_[new_record];
 }
 
+// FIXME: to kompletnie nie jest tak jak powinno
 template<class Type>
-Csv BaseFreqCounter<Type>::export_to_csv(std::wstring (*transformer)(Type)) {
+Csv FreqCounter<Type>::export_to_csv(std::wstring (*transformer)(Type)) {
     // ustawiamy wlasnosci naszego obiektu Csv
-    Csv::Config;
+    Csv::Config config;
     config.number_of_columns = 2;
 
+    // zapisujemy policzone przez nas dane jako wektor ktory mozemy dac do konstruktora Csv
     std::vector<std::wstring> converted;
     for (auto &field : data_) {
+        // zamieniamy pierwsze pole na string za pomoca podanej przez uzytkownika
         converted.push_back(transformer(field.first));
+        // zamieniamy liczbe wystapien na string zeby moc z niej zrobic Csv
         converted.push_back(std::to_wstring(field.second));
     }
-    return converted;
+
+    // FIXME there is aa copy there i should try to avoid
+    // return created Csv file
+    return Csv(converted, config);
 }
 
 
-template<class Type>
-class FreqCounter
-        : BaseFreqCounter<Type> {
-public:
-};
-
-
-#endif //FREQUENCY_COUNTER_V2_FREQCOUNTER_H
+#endif// FREQUENCY_COUNTER_V2_FREQCOUNTER_H
