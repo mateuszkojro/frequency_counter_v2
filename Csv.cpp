4c3666694e10db9b4d60a2209aa14047958645f1
Mateusz Kojro <mateuszkojro@outlook.com>
2021-02-06 00:49:50 +0100

    Implementing Csv

--- Csv.cpp
+++ Csv.cpp
@@ -1,5 +1,91 @@
-//
-// Created by mateu on 2/2/2021.
-//
 
 #include "Csv.h"
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+Csv::Csv(const std::vector<std::wstring> &data, const Config &config) {
+    config_ = config;
+    data_ = data;
+}
+
+void Csv::export_csv(const std::string &path) {
+
+    std::wfstream file;
+    file.open(path, std::ios::out);
+
+    // pozwala nam uniknac wielokrotnego sprawdzania w petli ustawien
+    std::wstring maybe_quote = (config_.has_quotes) ? std::wstring(L"\"", 1) : L"";
+
+    for (unsigned i = 0; i < data_.size(); i++) {
+
+        // zapisujemy pole do pliku
+        file << maybe_quote;
+        file << data_[i];
+        file << maybe_quote;
+
+        // w standardowym pliku CSV nie powinno byc przecinka po ostatnim elemencie
+        bool is_last_element = i == data_.size() - 1;
+        if (!is_last_element) {
+            file << L',';//config_.separator;
+        }
+
+        // standard pliow CSV wymaga braku znaku konca lini po ostatnim rzedzie
+        bool is_end_of_row =
+                i % config_.number_of_columns == config_.number_of_columns - 1;
+        if (!is_last_element && is_end_of_row) {
+            file << '\n';
+        }
+    }
+}
+
+void Csv::read_csv(const std::string &path) {
+
+    std::wfstream file;
+    file.open(path, std::ios::in);
+
+    std::wstring value;
+    int i = 0;
+    while (file.good()) {
+        // getline przyjmuje separator from config, jako separator kolejnych pol
+        std::getline(file, value, config_.separator);
+        // add to inside storage next value from a file
+        data_.push_back(value);
+        bool is_last_element_on_line = i % config_.number_of_columns;
+        // z uwaagi na dzialanie getline musimy recznie usunac newline from the end of the line
+        if (is_last_element_on_line) {
+            wchar_t trash;
+            file >> trash;
+            i = 0;
+        }
+        i++;
+    }
+}
+
+std::wstring &Csv::operator()(unsigned column, unsigned row) {
+    // @vstd::vector.at() sprawdza za nas czy adres jest w zakresie jezeli
+    // nie wyrzuca std::out_of_range rownanie dostepu pozwala nam symulowac
+    // istnienie tablicy 2 wymiarowej
+    return data_.at(column + row * config_.number_of_columns);
+}
+
+unsigned Csv::number_of_columns() const { return config_.number_of_columns; }
+
+unsigned Csv::number_of_rows() {
+    // liczba rekordow jest rowna liczbie wszystkich danych podzelonych przez ilosc kolumn do ktorych zapuisujemy
+    if (config_.number_of_columns) throw std::runtime_error("Cannot get the number of rows when numbers of columns is 0");
+    return (unsigned) data_.size() / (unsigned) config_.number_of_columns;
+}
+
+const Csv::Config &Csv::get_config() const {
+    return config_;
+}
+
+void Csv::set_config(const Csv::Config &config) {
+    config_ = config;
+}
+
+const std::vector<std::wstring> &Csv::get_data() const {
+    return data_;
+}
--- Csv.h
+++ Csv.h
@@ -1,14 +1,77 @@
-//
-// Created by mateu on 2/2/2021.
-//
-
 #ifndef FREQUENCY_COUNTER_V2_CSV_H
 #define FREQUENCY_COUNTER_V2_CSV_H
 
+/**
+	@Autor: Mateusz Kojro
+	@details: Prosta implementacja obslugi plikow Csv (Zapisywanie, Odczytywanie, Dostep do danych)
+	@date: 27.01.2021
+	Zmiany:
+		- 29.01: usuwam nie potrzebne elementy z poprzedniej implementacji
+		- 2.02: dodaje dodatkowe komentarze
+*/
+
+#include <string>
+#include <vector>
+#include <stdexcept>
 
 class Csv {
+public:
+    struct Config {
+        /**
+         * @brief separator beetwen subsequnet columns
+         */
+        wchar_t separator = L',';
+        /**
+         * @brief does first row consist of headers
+         */
+        bool has_headers = false;
+        /**
+         * @brief are the values surrounded with quotes
+         */
+        bool has_quotes = false;
+        /**
+         * @brief nuber of columns
+         */
+        unsigned number_of_columns = 0;
+    };
+
+public:
+    Csv() = default;
+    ~Csv() = default;
+    Csv &operator=(const Csv &) = default;
+    Csv(const Csv &) = default;
+    Csv(const std::vector<std::wstring> &, const Config &);
+
+public:
+    /**
+     * @brief reads csv formated file
+     */
+    void read_csv(const std::string &);
+
+    /**
+     * @brief exports data to csv formated file
+     */
+    void export_csv(const std::string &);
+    unsigned number_of_columns() const;
+    unsigned number_of_rows();
+
+    /**
+     * @brief acces element of given coordianates
+     * @param x - column counted from 0
+     * @param y - row counted from 0
+     * @return reference to elelment on position x, y
+     */
+    std::wstring &operator()(unsigned, unsigned);
+
+    const std::vector<std::wstring> &get_data() const;
+    const Config &get_config() const;
+    void set_config(const Config &config);
+
+private:
+    std::vector<std::wstring> data_;
+    Config config_;
 
 };
 
 
-#endif //FREQUENCY_COUNTER_V2_CSV_H
+#endif//FREQUENCY_COUNTER_V2_CSV_H
